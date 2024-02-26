40966847ca8a7d2aca7e793396331ce6c9fa9bc2
Kiwi <allanvillegas35@gmail.com>
2024-02-26 23:48:18 +0800

    refactor: change table structure

--- uc_css_monitoring.sql
+++ uc_css_monitoring.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2024 at 08:16 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Feb 26, 2024 at 04:47 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,56 +24,63 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `staff`
---
-
-CREATE TABLE `staff` (
-  `id` int(11) NOT NULL,
-  `first_name` int(11) NOT NULL,
-  `last_name` int(11) NOT NULL,
-  `email` int(11) NOT NULL,
-  `password` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `students`
 --
 
 CREATE TABLE `students` (
-  `id` int(11) NOT NULL,
+  `id` int(80) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
+  `id_no` int(255) NOT NULL,
   `course` varchar(255) NOT NULL,
-  `year` int(11) NOT NULL,
+  `year` varchar(80) NOT NULL,
   `no_sessions` int(11) NOT NULL DEFAULT 30,
-  `id_no` int(80) NOT NULL,
-  `created_at` int(80) NOT NULL DEFAULT current_timestamp(),
-  `updated_at` int(80) NOT NULL DEFAULT current_timestamp(),
-  `active` int(80) DEFAULT 1,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `active` int(60) NOT NULL DEFAULT 1,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `first_name`, `last_name`, `course`, `year`, `no_sessions`, `id_no`, `created_at`, `updated_at`, `active`, `email`, `password`) VALUES
-(1, 'Allan', 'Villegas', 'BSIT', 3, 30, 123123, 2147483647, 2147483647, 1, 'allanvillegas35@gmail.com', '263fec58861449aacc1c328a4aff64aff4c62df4a2d50b3f207fa89b6e242c9aa778e7a8baeffef85b6ca6d2e7dc16ff0a760d59c13c238f6bcdc32f8ce9cc62');
+INSERT INTO `students` (`id`, `email`, `password`, `first_name`, `last_name`, `id_no`, `course`, `year`, `no_sessions`, `active`, `created_at`, `updated_at`) VALUES
+(18, 'allanvillegas35@gmail.com', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'Allan', '3123123', 213123, '3123213', '12312', 30, 1, '2024-02-19 22:26:24', '2024-02-19 22:26:24'),
+(19, 'allanvillegas35@gmail.com1', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'Allan', '3123123', 213123, '3123213', '12312', 30, 1, '2024-02-19 22:26:56', '2024-02-19 22:26:56'),
+(20, 'allanvillegas699@gmail.com', '263fec58861449aacc1c328a4aff64aff4c62df4a2d50b3f207fa89b6e242c9aa778e7a8baeffef85b6ca6d2e7dc16ff0a760d59c13c238f6bcdc32f8ce9cc62', 'Allan', 'Villegas', 21419023, '1st', '1st', 30, 1, '2024-02-22 00:14:54', '2024-02-22 00:14:54'),
+(21, 'xaenic@gmail.com', '263fec58861449aacc1c328a4aff64aff4c62df4a2d50b3f207fa89b6e242c9aa778e7a8baeffef85b6ca6d2e7dc16ff0a760d59c13c238f6bcdc32f8ce9cc62', 'Allan', 'Villegas', 213123, '1st', '1st', 30, 1, '2024-02-26 23:24:01', '2024-02-26 23:24:01'),
+(22, 'allanvill2egas35@gmail.com', '7fcf4ba391c48784edde599889d6e3f1e47a27db36ecc050cc92f259bfac38afad2c68a1ae804d77075e8fb722503f3eca2b2c1006ee6f6c7b7628cb45fffd1d', 'Allan', 'Villegas', 213123, '1st', '1st', 30, 1, '2024-02-26 23:44:14', '2024-02-26 23:44:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL DEFAULT 'staff',
+  `active` int(50) NOT NULL DEFAULT 1,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `password`, `role`, `active`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'Admin', 'admin@gmail.com', '7fcf4ba391c48784edde599889d6e3f1e47a27db36ecc050cc92f259bfac38afad2c68a1ae804d77075e8fb722503f3eca2b2c1006ee6f6c7b7628cb45fffd1d', 'admin', 1, '2024-02-26 23:41:42', '2024-02-26 23:41:42');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `staff`
---
-ALTER TABLE `staff`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `students`
@@ -82,19 +89,25 @@ ALTER TABLE `students`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `user`
 --
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for table `staff`
+-- AUTO_INCREMENT for dumped tables
 --
-ALTER TABLE `staff`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
+  MODIFY `id` int(80) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
