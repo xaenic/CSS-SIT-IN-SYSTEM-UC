6de0d58fcc4aa5eb7f1271f15cb6de80f499c91d
Kiwi <allanvillegas35@gmail.com>
2024-02-28 16:40:05 +0800

    config: change app name

--- config.php
+++ config.php
@@ -9,29 +9,27 @@
 /* 
 PROJECT URL
 */
- 
+
 // define ('_SITE_URL', 'localhost');
 
 
-define ('APPLICATION_NAME','SitHub');
+define('APPLICATION_NAME', 'CCSLab');
 //DATABASE CONFIGURATION 
 
 
 
 $database = array(
-'host' => "localhost",
-'db' => "uc_css_monitoring",
-'user' => "root",
-'pass' => ""
+  'host' => "localhost",
+  'db' => "uc_css_monitoring",
+  'user' => "root",
+  'pass' => ""
 );
 
 // SITE CONFIGURATION 
 
 // $site_config = array(
-	
+
 //     'items_page' => 20, // Number Of Items To Display Per Page (Minimum 5)
 //     'items_featured' => 12, // Number Of Featured Items To Display (Minimum 4)
 //     'items_recent' => 10, // Number Of Recent Items To Display (Minimum 5)
 // );
-
-?>
