7eb1f125e9b2776bfbc59f006217cb5ad242a11b
Kiwi <allanvillegas35@gmail.com>
2024-02-28 16:40:33 +0800

    wip: implementing lab page

---
+++ admin/views/labs.view.php
@@ -0,0 +1,17 @@
+<div class="mt-10 flex flex-col gap-6">
+    <div class="border-b pb-2 border-gray-700">
+        <h1 class="text-xl">List of Laboratories</h1>
+    </div>
+    <div class="flex justify-end">
+        <a href="/actions/new_lab" class="px-3 p-3 bg-gray-900 rounded-md hover:bg-blue-900 duration-200 transition-colors flex items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z" />
+            </svg>
+            <span>Add New</span>
+        </a>
+    </div>
+
+    <div>
+
+    </div>
+</div>
