ff07ccd1fe3fe7a79cf1a38840285592c5a015d1
Kiwi <allanvillegas35@gmail.com>
2024-02-28 16:40:18 +0800

    feat: added a breadcrumb

--- shared/topbar.php
+++ shared/topbar.php
@@ -7,7 +7,25 @@
             </svg>
             <input type="text" placeholder="Search..." class="w-full px-3 p-2 text-sm outline-none rounded-xl border-none rounded-lg" />
         </div> -->
-        <span class="text-white">Dashboard</span>
+        <div class="flex gap-2 items-center">
+            <?php
+            if (isset($_SESSION['role'])) {
+                echo '<a href="/admin/dashboard.php" class="text-white">Dashboard</a>';
+            } else echo '<a href="/dashboard.php" class="text-white">Dashboard</a>';
+
+
+
+            if ($page_title != 'Dashboard') {
+                echo '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" class="text-white" viewBox="0 0 16 16">
+                <path fill="currentColor" d="m5.157 13.069l4.611-4.685a.546.546 0 0 0 0-.768L5.158 2.93a.552.552 0 0 1 0-.771a.53.53 0 0 1 .759 0l4.61 4.684a1.65 1.65 0 0 1 0 2.312l-4.61 4.684a.53.53 0 0 1-.76 0a.552.552 0 0 1 0-.771" />
+            </svg>
+            <span class=" text-sm text-slate-300">' . $page_title . '</span>';
+            }
+            ?>
+
+
+
+        </div>
     </div>
     <div>
         <a href="/profile">
