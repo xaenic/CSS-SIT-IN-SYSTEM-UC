d25bd9e58b7b0b609a6872257ca49ff6b3315230
xaenic <allanvillegas35@gmail.com>
2024-03-01 03:08:00 +0800

    wip: implementing profile page

---
+++ profile.php
@@ -0,0 +1,30 @@
+<?php
+$page_title = "Dashboard";
+$parent = [""];
+require "core.php";
+require './shared/header.php';
+if (!isset($_SESSION['email']))
+    header('Location: /login');
+if (isset($_SESSION['role']))
+    header('Location: /admin/dashboard');
+
+$connect = connect($database);
+
+$statement = $connect->prepare("SELECT * FROM students WHERE email = :email AND active = 1 ");
+$statement->execute(array(
+    ':email' => $_SESSION['email'],
+));
+
+$user = $statement->fetch();
+?>
+
+
+<div class="flex min-h-screen bg-gradient-to-t from-slate-900 to-gray-900 min-h-screen ">
+    <!-- Sidebar -->
+    <?php require './shared/sidebar.php'; ?>
+    <!-- Main Content -->
+    <main class="flex-1 p-4 lg:ml-64  lg:pt-5">
+        <?php require './shared/topbar.php'; ?>
+        <?php require './views/dashboard.view.php'; ?>
+    </main>
+</div>
